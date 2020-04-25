d2d4fca279a5f1b50819655c17af804892088842
lexuanquynh <quynhlxbkhn@gmail.com>
2020-04-25 12:15:49 +0900

    add new project

---
+++ app/src/main/java/com/zentedu/loginfirebasesample/view/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.zentedu.loginfirebasesample.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zentedu.loginfirebasesample.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
